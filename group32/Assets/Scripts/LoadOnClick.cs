1329c6e79b4e636144fbff50ce25f29d80bd1798
Dylan Hobbs <dhobbs@tcd.ie>
2016-02-10 14:00:55 +0000

    Game selection screen

---
+++ group32/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+	/**
+	 * @param scene Index of the scene in build settings that is to be loaded
+	 */
+	public void LoadScene(int scene){
+		SceneManager.LoadScene (scene);
+	}
+}
