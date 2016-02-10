497471e6fe2b2af075b7aa65ee8b1d93a090bbf4
Dylan Hobbs <dhobbs@tcd.ie>
2016-02-10 14:37:14 +0000

    Adding basic loading screen

--- group32/Assets/Scripts/LoadOnClick.cs
+++ group32/Assets/Scripts/LoadOnClick.cs
@@ -4,10 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour {
 
+	public GameObject loadingImage;
+
 	/**
 	 * @param scene Index of the scene in build settings that is to be loaded
 	 */
 	public void LoadScene(int scene){
+		loadingImage.SetActive (true);
 		SceneManager.LoadScene (scene);
 	}
 }
