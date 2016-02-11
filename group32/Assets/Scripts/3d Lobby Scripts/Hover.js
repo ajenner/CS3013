9f0db0369be8a7002c1751b82caf761ce15c1524
Dylan Hobbs <dhobbs@tcd.ie>
2016-02-11 00:46:39 +0000

    Added 3d lobby scene and deleted old project

---
+++ group32/Assets/Scripts/3d Lobby Scripts/Hover.js
@@ -0,0 +1,23 @@
+﻿import UnityEngine.SceneManagement;
+#pragma strict
+
+var levelToLoad : String;
+var soundhover : AudioClip;
+var soundSelection : AudioClip;
+var QuitButton : boolean = false;
+ 
+ function OnMouseEnter(){
+     GetComponent.<AudioSource>().PlayOneShot(soundhover);
+ }
+ 
+ function OnMouseUp(){
+     GetComponent.<AudioSource>().PlayOneShot(soundSelection);
+     yield new WaitForSeconds(0.35);
+     if(QuitButton){
+         Application.Quit();
+     }
+     else{
+         SceneManager.LoadScene (levelToLoad);
+     }
+ }
+ 
