3b667ebaee533449f26059185181117548e8a8fc
Dylan Hobbs <dhobbs@tcd.ie>
2016-02-20 18:51:47 +0000

    Added grid system and basic scripting for the memory box game

---
+++ group32/Assets/Scripts/BoxMemoryGame/Tile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+	//Tile Pair
+	Tile pair;
+
+	public TextMesh displayText;
+
+	//Gameplay bools
+	public bool occupied = false;
+	public bool selected = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
