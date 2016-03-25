91eee686d24bcaf51771196e7c9d19deb54283c8
Dylan Hobbs <dhobbs@tcd.ie>
2016-03-25 16:23:01 +0000

    Start of maze generation, currently 2d tiles with delay in generation

---
+++ group32/Assets/Scripts/MazeGame/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Maze mazeFab;
+	private Maze myMaze;
+
+	private void Start () {
+		BeginGame();
+	}
+
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			RestartGame();
+		}
+	}
+
+	private void BeginGame () {
+		myMaze = Instantiate (mazeFab) as Maze;
+		StartCoroutine (myMaze.generate ());
+	}
+
+	private void RestartGame () {
+		Destroy(myMaze.gameObject);
+		StopAllCoroutines ();
+		BeginGame ();
+	}
+}
---
+++ group32/Assets/Scripts/MazeGame/Maze.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze : MonoBehaviour {
+	public int sizeX, sizeZ;
+	public MazeCell cellFab;
+	private MazeCell[,] cells;
+
+	public float generateDelay;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator generate(){
+		WaitForSeconds delay = new WaitForSeconds (generateDelay);
+		cells = new MazeCell[sizeX,sizeZ];
+		for (int x = 0; x < sizeX; x++) {
+			for (int z = 0; z < sizeZ; z++) {
+				yield return delay;
+				createCell (x, z);
+			}
+		}
+	}
+
+	private void createCell(int x, int z){
+		MazeCell newCell = Instantiate(cellFab) as MazeCell;
+		cells [x, z] = newCell;
+		newCell.name = "Maze Cell " + x + ", " + z;
+		newCell.transform.parent = transform;
+		newCell.transform.localPosition = new Vector3 (x - sizeX * 0.5f + 0.5f, 0f, z - sizeZ * 0.5f + 0.5f);
+	}
+}
